6f8d1772e0411b1e4c4aab47b44bfa0688125e78
pramod <kpramod8095@allstate.com>
2022-06-29 19:27:21 +0530

    main

--- src/Component/RandomUser.js
+++ src/Component/RandomUser.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Incre.css";
 export default function RandomUser({ API }) {
-  const [user, setUser] = useState();
   const [pagenumber, setPagenumber] = useState(1);
   const [userinfo, setUserinfo] = useState([]);
   const getUserData = async (number) => {
@@ -25,7 +24,6 @@ export default function RandomUser({ API }) {
 
   const load = () => {
     getUserData(pagenumber).then((user) => {
-      setUser(JSON.stringify(user, null, 2));
       const newResults = [...userinfo, ...user.results];
       setUserinfo(newResults);
       setPagenumber(user.info.page + 1);
